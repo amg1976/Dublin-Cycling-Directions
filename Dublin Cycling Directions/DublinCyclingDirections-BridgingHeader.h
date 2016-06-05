0eebcc170de058fa32b512ea34e81521c07775a3
Adriano Goncalves <agoncalves@zendesk.com>
2016-06-05 22:14:03 +0100

    Using ReactiveKit

---
+++ Dublin Cycling Directions/DublinCyclingDirections-BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  DublinCyclingDirections-BridgingHeader.h
+//  Dublin Cycling Directions
+//
+//  Created by Adriano Goncalves on 05/06/2016.
+//  Copyright © 2016 Adriano Goncalves. All rights reserved.
+//
+
+#ifndef DublinCyclingDirections_BridgingHeader_h
+#define DublinCyclingDirections_BridgingHeader_h
+
+
+#endif /* DublinCyclingDirections_BridgingHeader_h */
